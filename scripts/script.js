b507cc4c09ffe42dbc95e42b31dbc63065b66756
Amir Napoletano <amirnapoletano@gmail.com>
2022-12-08 15:35:55 +0100

    voeg toe cheklist
    pass toe Javascript

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+let checklist = document.querySelectorAll(".richtlijnen input[type=checkbox]");
+let progression = document.querySelector("#waarneembaar-progress");
+console.log(checklist.length);
+
+checklist.forEach(function (checkbox) {
+    checkbox.addEventListener('click', function ()
+    {
+        progression.value = document.querySelectorAll(".richtlijnen input[type=checkbox]:checked").length;
+    })
+})
+
