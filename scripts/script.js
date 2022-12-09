774ca900bf9ab9fcfa76a43c0125d3e042f7a80f
Amir Napoletano <amirnapoletano@gmail.com>
2022-12-09 14:02:37 +0100

    Comments toegevoegd in JS

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,9 @@
+/*checklist die haalt alle checkboxes op van de richtlijnen*/
 let checklist = document.querySelectorAll(".richtlijnen input[type=checkbox]");
+//progressie balk 
 let progression = document.querySelector("#waarneembaar-meter");
 console.log(checklist.length);
-
+//zodra een checkbox wordt aangeklikt, wordt de progressie geupdated
 checklist.forEach(function (checkbox) {
   checkbox.addEventListener("click", function () {
     progression.value = document.querySelectorAll(
