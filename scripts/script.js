759f3edb5afdaa43d0ff4bbac96a712ffbe36759
Amir Napoletano <amirnapoletano@gmail.com>
2022-12-19 18:53:27 +0100

    footer toegevoegd

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,12 @@
 /*checklist die haalt alle checkboxes op van de richtlijnen*/
+
 let checklist = document.querySelectorAll(".richtlijnen input[type=checkbox]");
 //progressie balk 
+
 let progression = document.querySelector("#waarneembaar-meter");
 console.log(checklist.length);
 //zodra een checkbox wordt aangeklikt, wordt de progressie geupdated
+
 checklist.forEach(function (checkbox) {
   checkbox.addEventListener("click", function () {
     progression.value = document.querySelectorAll(
@@ -11,3 +14,17 @@ checklist.forEach(function (checkbox) {
     ).length;
   });
 });
+
+
+
+//formulier
+let contactForm = document.querySelector(".vragen-formulier")
+let buttonForm = document.querySelector(".display-vragen-formulier")
+console.log(buttonForm)
+
+buttonForm.addEventListener('click', displayForm)
+
+function displayForm() {
+    contactForm.classList.add('vragen-formulier-display')
+    buttonForm.classList.add('undisplay-vragen-formulier')
+}
